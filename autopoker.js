a18c39a21c6c2778045f89af28251a0d6e8d009b
Felds Liscia <felds@seepix.com.br>
2013-09-05 11:04:32 -0300

    add new version and add todos

--- autopoker.js
+++ autopoker.js
@@ -1,6 +1,24 @@
-function poke() {
-  var els = $x('//a[contains(@ajaxify, "pokeback")]')
-  for (i in els) els[i].click()
-}
+var pokes = [];
 
-setInterval(poke, 10000);
+function poke() {
+    // @TODO use snapshot type (XPathResult.ORDERED_NODE_SNAPSHOT_TYPE)
+    var els = document.evaluate('//li[contains(@id,"poke_")]', document), // select items
+        el, button
+
+    try {
+        while (el = els.iterateNext()) {
+            // select the button inside the poke element
+            button = document.evaluate('//a[contains(@ajaxify, "pokeback")]', el)
+
+            if (button) {
+                // poke
+                button.click()
+
+                // @TODO add a poke to user
+                // @TODO notify
+            }
+        }
+    } catch (e) {
+        // @TODO handle errors
+    }
+}
